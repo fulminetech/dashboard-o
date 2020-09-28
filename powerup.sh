fed7ff44db19690e80d2f9b254beda7e9c6b80ad
marutimuthu <marutimuthu@gmail.com>
2020-09-28 16:47:13 +0530

    Update powerup.sh

--- powerup.sh
+++ powerup.sh
@@ -2,7 +2,15 @@
 echo "[ Starting dashboard ]"
 
 #Expand filestorage
-sudo raspi-config --expand-rootfs && sudo apt install npm
+sudo raspi-config --expand-rootfs 
+
+#Install npm
+sudo apt install npm
+
+#Download node  installer (Will have to delete old one manually) Run ``sudo n`` and install latest node version 
+sudo npm install n pm2 -g
+
+#Download node --> https://docs.influxdata.com/influxdb/v1.8/introduction/install/
 
 #Clone Dashboard
 cd ~
@@ -10,10 +18,10 @@ git clone https://github.com/fulminetech/dashboard-o
 
 # To start the service
 cd dashboard-o
-npm run live
+npm run install
 
 # Spawn and daemonise systemctl
-sudo systemctl start influxd && sudo systemctl enable influxd 
+#sudo systemctl start influxd && sudo systemctl enable influxd 
 
 #sleep 2
 #sudo reboot -fn
