d4ee1121232cc7f202d6f0f35a381f7a32531e59
fulminetech <71808779+fulminetech@users.noreply.github.com>
2020-09-28 16:07:21 +0530

    Update powerup.sh

--- powerup.sh
+++ powerup.sh
@@ -2,23 +2,18 @@
 echo "[ Starting dashboard ]"
 
 #Expand filestorage
-# sudo raspi-config --expand-rootfs && sudo apt install npm
+sudo raspi-config --expand-rootfs && sudo apt install npm
 
 #Clone Dashboard
 cd ~
-git clone https://github.com/marutimuthu/dashboard
-
-#Remove old modules
-rm -rf ~/dashboard/node_modules/
-
-#Install fresh modules
-cd ~/dashboard/
+git clone https://github.com/fulminetech/dashboard-o
 
 # To start the service
+cd dashboard-o
 npm run live
 
 # Spawn and daemonise systemctl
-# sudo systemctl start influxd && sudo systemctl enable influxd 
+sudo systemctl start influxd && sudo systemctl enable influxd 
 
 #sleep 2
 #sudo reboot -fn
